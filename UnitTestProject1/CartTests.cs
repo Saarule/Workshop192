850f791fa9ec2b1eec131c09bd32809b58741c90
Saarule <saaryazdi@gmail.com>
2019-05-15 01:23:12 +0300

    minor fix

--- UnitTestProject1/CartTests.cs
+++ UnitTestProject1/CartTests.cs
@@ -8,28 +8,20 @@ namespace UnitTestProject1
     [TestClass]
     public class CartTests
     {
-        Workshop192.System system = Workshop192.System.GetInstance();
-        StoreOwner storeOwner;
-        Product product1 = new Workshop192.MarketManagment.Product(1, 100, "product");
-        Product product2;
-
-        [TestInitialize]
-        public void Init()
-        {
-            system.AddUser("user1", "12345");
-            system.OpenStore("myStore", system.GetUser("user1", "12345"));
-           
-        }
 
         [TestMethod]
         public void AddProductTest()
         {
+            Workshop192.System system = Workshop192.System.GetInstance();
+            system.AddUser("user1", "12345");
+            system.OpenStore("myStore", system.GetUser("user1", "12345"));
             User user = system.GetUser("user1", "12345");
-            storeOwner = user.GetStores().First.Value;
-            product1 = new Workshop192.MarketManagment.Product(1,100,"product");
+            StoreOwner storeOwner = user.GetStores().First.Value;
+
+            Product product1 = new Workshop192.MarketManagment.Product(1,100,"product");
             storeOwner.AddProduct(product1);
             Assert.IsTrue(storeOwner.GetStore().GetProducts().Count == 1);
-            product2 = new Workshop192.MarketManagment.Product(2, 100, "product2");
+            Product product2 = new Workshop192.MarketManagment.Product(2, 100, "product2");
             storeOwner.AddProduct(product2);
             Assert.IsTrue(storeOwner.GetStore().GetProducts().Count == 2);
         }
@@ -37,9 +29,28 @@ namespace UnitTestProject1
         [TestMethod]
         public void RemoveProductTest()
         {
-            storeOwner.RemoveProduct(product1);
-            //sstoreOwner.(product1);
+            Workshop192.System system = Workshop192.System.GetInstance();
+            system.AddUser("user1", "12345");
+            system.OpenStore("myStore", system.GetUser("user1", "12345"));
+            User user = system.GetUser("user1", "12345");
+            StoreOwner storeOwner = user.GetStores().First.Value;
+
+            Product product1 = new Workshop192.MarketManagment.Product(1, 100, "product");
+            storeOwner.AddProduct(product1);
             Assert.IsTrue(storeOwner.GetStore().GetProducts().Count == 1);
+
+            Product product2 = new Workshop192.MarketManagment.Product(2, 100, "product2");
+            storeOwner.AddProduct(product2);
+            Assert.IsTrue(storeOwner.GetStore().GetProducts().Count == 2);
+
+            storeOwner.RemoveProduct(product1);
+            Assert.IsTrue(storeOwner.GetStore().GetProducts().Count == 1);
+
+            storeOwner.RemoveProduct(product2);
+            Assert.IsTrue(storeOwner.GetStore().GetProducts().Count == 0);
+
+
+
 
         }
 
