18592490371b13bae95902c7a7f1638f24556408
hben <hben@post.bgu.ac.il>
2019-05-19 18:29:08 +0300

    fix merge

---
+++ Workshop192/DeliverySystemReal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workshop192.MarketManagment;
+
+namespace Workshop192
+{
+    public class DeliverySystemReal : DeliverySystemInterface
+    {
+        public bool Deliver(string name, string address, LinkedList<Cart> carts)
+        {
+            return true;
+        }
+    }
+}
