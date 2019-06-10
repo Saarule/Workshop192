4e48f3e49707e66e7702875a9660912ba142b1e4
hben <hben@post.bgu.ac.il>
2019-06-10 08:53:16 +0300

    more tests

--- Workshop192/MarketManagment/Store.cs
+++ Workshop192/MarketManagment/Store.cs
@@ -42,6 +42,53 @@ namespace Workshop192.MarketManagment
             return inventory.EditProduct(productId, name, category, price, amount);
         }
 
+        public void AddDiscountPolicy(PolicyComponent policy, int discount)
+        {
+            discountPolicies.AddLast(new Tuple<PolicyComponent, int>(policy, discount));
+        }
+
+        public void AddSellingPolicy(PolicyComponent policy)
+        {
+            sellingPolicies.AddLast(policy);
+        }
+
+        public bool RemoveDiscountPolicy(int policyId)
+        {
+            if (discountPolicies.Count < policyId)
+                return false;
+            return discountPolicies.Remove(discountPolicies.ElementAt(policyId));
+        }
+
+        public bool RemoveSellingPolicy(int policyId)
+        {
+            if (sellingPolicies.Count < policyId)
+                return false;
+            return sellingPolicies.Remove(sellingPolicies.ElementAt(policyId));
+        }
+
+        public void SetDiscountMinimum(int userId, Cart cart)
+        {
+            foreach (Tuple<PolicyComponent, int> policy in discountPolicies)
+                if (policy.Item1.Validate(userId, cart))
+                {
+                    int sum = 0;
+                    foreach (KeyValuePair<Product, int> productAmount in cart.GetProducts())
+                    {
+                        sum += productAmount.Key.GetPrice() * productAmount.Value * (100 - policy.Item2) / 100;
+                    }
+                    if (sum < cart.GetCartSum())
+                        cart.SetSum(sum);
+                }
+        }
+
+        public bool CheckSellingPolicies(int userId, Cart cart)
+        {
+            foreach (PolicyComponent policy in sellingPolicies)
+                if (!policy.Validate(userId, cart))
+                    return false;
+            return true;
+        }
+
         public string GetName()
         {
             return name;
