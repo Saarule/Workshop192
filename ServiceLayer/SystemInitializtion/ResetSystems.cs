2c9eedf7bdf18379403f9c60e05ab7d0c8d61103
אוראל קקון <orelkak@post.bgu.ac.il>
2019-06-12 16:11:02 +0300

    ...

---
+++ ServiceLayer/SystemInitializtion/ResetSystems.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.SystemInitializtion
+{
+    public class ResetSystems
+    {
+        public static void ResetSystem()
+        {
+            Workshop192.MarketManagment.System.Reset();
+            Workshop192.UserManagment.AllRegisteredUsers.Reset();
+        }
+    }
+}
